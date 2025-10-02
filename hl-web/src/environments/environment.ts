8db10a84dc89a5492fa11fd633f015728457f207
Juan Fuentes <juan_fuentes@cable.comcast.com>
2025-10-01 21:41:00 -0400

    updating new api url https with subdomain

--- hl-web/src/environments/environment.ts
+++ hl-web/src/environments/environment.ts
@@ -2,5 +2,7 @@
 export const environment = {
   production: false,
   //apiUrl: 'http://localhost:8080/api'
-  apiUrl: 'http://hl-api-alb-1496841054.us-east-1.elb.amazonaws.com/api'
+  // apiUrl: 'http://hl-api-alb-1496841054.us-east-1.elb.amazonaws.com/api'
+  apiUrl: 'https://hl-api.demopitch.click/api',
+
 };
