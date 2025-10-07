c32801308e44303481e8f54bd1ab5652f3241f04
Juan Fuentes <juan_fuentes@cable.comcast.com>
2025-10-06 21:08:23 -0400

    implementing secrets management, multi solution.

--- terraform/hl-infra/modules/app_secrets/main.tf
+++ terraform/hl-infra/modules/app_secrets/main.tf
@@ -36,13 +36,3 @@ resource "aws_secretsmanager_secret_version" "secret" {
   secret_id     = aws_secretsmanager_secret.secret[0].id
   secret_string = var.secret_value
 }
-
-output "identifier" {
-  description = "Name or ARN to identify the secret from the application"
-  value       = var.use_secrets_manager ? aws_secretsmanager_secret.secret[0].name : aws_ssm_parameter.secret[0].name
-}
-
-output "arn" {
-  description = "ARN of the underlying secret"
-  value       = var.use_secrets_manager ? aws_secretsmanager_secret.secret[0].arn : aws_ssm_parameter.secret[0].arn
-}
